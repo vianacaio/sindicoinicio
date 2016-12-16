f36be4caa70946502595cf20d94876b0ebf6dbaa
Caio Viana <vcaiocezar@gmail.com>
2016-12-16 16:02:39 -0300

    versão com login e dois tipos de usuários

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 1)
+        	->states('admin')
+        	->create([
+        		'name' => 'Caio Viana',
+        		'email' => 'admin@user.com'
+
+        		]);
+
+
+        factory(App\User::class, 1)
+                ->create([
+                'name' => 'Client da Silva',
+                'email' => 'client@user.com'
+
+                ]);   
+
+
+    }
+}
